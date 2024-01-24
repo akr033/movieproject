4584c6ffe05acf677e9f9b27c0eb6f207b1e2c53
akr033 <albinkroby72@gmail.com>
2024-01-24 18:20:41 +0530

    first commit

--- movieapp/views.py
+++ movieapp/views.py
@@ -25,6 +25,7 @@ def add_movie(request):
         img=request.FILES['image']
         movie=Movie(name=name,desc=desc,year=year,img=img)
         movie.save()
+        return redirect('/')
     return render(request,'add.html')
  
     
